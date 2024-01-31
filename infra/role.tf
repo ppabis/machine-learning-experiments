e17244dcd807d11a17c33f372175245ab97db9a0
ppabis <piotr@pabis.eu>
2024-01-31 21:24:42 +0100

    Role for SageMaker Notebooks.

---
+++ infra/role.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "sagemaker" {
+  name = "SageMakerNotebookRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "sagemaker.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker" {
+  role       = aws_iam_role.sagemaker.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
