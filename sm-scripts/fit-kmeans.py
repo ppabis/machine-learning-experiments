39837175fe2b60c5c2bb89134a8da8c7301f74e7
ppabis <piotr@pabis.eu>
2024-02-03 15:06:54 +0100

    If there's a scaler for the input data, copy it over and pack with the model

--- sm-scripts/fit-kmeans.py
+++ sm-scripts/fit-kmeans.py
@@ -1,7 +1,6 @@
-import os
+import os, shutil, pathlib, joblib
 import pandas as pd
 from sklearn.cluster import KMeans
-import joblib
 
 if __name__ == "__main__":
     input_file = os.path.join(os.environ['SM_CHANNEL_TRAIN'], os.environ['SM_INPUT_DATA_FILE'])
@@ -16,3 +15,11 @@ if __name__ == "__main__":
     model = KMeans(n_clusters=6).fit(csv)
 
     joblib.dump(model, model_file)
+
+    # If the input file also has scaler weights, keep them close to the model too
+    # So if the input training file is named "records-2022.csv", the scaler should be named "records-2022.scaler.joblib"
+    # And the copied weights for the scaler will be "my-kmeans-model.scaler.joblib" for model named "my-kmeans-model.joblib
+    scaler_source = os.path.join(os.environ['SM_CHANNEL_TRAIN'], pathlib.Path(os.environ['SM_INPUT_DATA_FILE']).stem + ".scaler.joblib")
+    if os.path.exists( scaler_source ):
+        scaler_dest = os.path.join(os.environ['SM_MODEL_DIR'], pathlib.Path(os.environ['SM_MODEL_FILE']).stem + ".scaler.joblib")
+        shutil.copy(scaler_source, scaler_dest)
